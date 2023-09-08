5214a720a31ecdf49e6440e048b277633c3b5252
Ethan Avera <averaeth26@7hills.org>
2023-09-08 12:56:38 -0400

    Began coding word converter.

---
+++ PigLatin.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PigLatin {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    
+    String word;
+    int len;
+    while (true) {
+        word = scan.nextLine();
+        if (word.toLowerCase().equals("quit")) {
+            break;
+        }
+        len = word.length();
+        if ("aeiou".contains(word.toLowerCase().charAt(0)))
+        word = word.substring()
+
+        
+    }
+    }
+}
