5fae10f90edc3e0cf307758447957e100570d588
Ethan Avera <averaeth26@7hills.org>
2023-09-08 14:59:00 -0400

    Finished phrase translator.

--- PigLatin.java
+++ PigLatin.java
@@ -10,17 +10,17 @@ public class PigLatin {
     String word;
     while (true) {
         System.out.print("Enter a word to be translated: ");
-        phrase = scan.nextLine().toLowerCase();
-        if (phrase.toLowerCase().equals("quit")) {
+        phrase = (scan.nextLine() + " ").toLowerCase();
+        if (phrase.toLowerCase().equals("quit ")) {
             System.out.println("Goodbye!");
             break;
         }
         len = phrase.length();
-        prevWord = 0;
+        prevWord = -1;
         for (int i = 0; i < len; i++) {
             word = "";
             if (phrase.charAt(i) == ' ') {
-                word = phrase.substring(prevWord, i);
+                word = phrase.substring(prevWord+1, i);
                 prevWord = i;
                 wordLen = word.length();
                 if (word.charAt(0) == 'a' || word.charAt(0) == 'e' || word.charAt(0) == 'i' || word.charAt(0) == 'o' || word.charAt(0) == 'u') {
